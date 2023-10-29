26d1700f1d7eeef1929722a24471aa473cb72f24
Amanda Barsanti <barsanti@Amandas-MacBook-Pro.local>
2023-10-29 19:02:48 -0400

    added delete

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -125,6 +125,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(ProductData => {
+    if (!ProductData){
+      res.status(404).json({message: 'No product found!'});
+      return;  
+    }
+  res.json(ProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
